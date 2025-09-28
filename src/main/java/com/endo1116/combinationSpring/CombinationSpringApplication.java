86905600f5190a6ceb76c374b235ad60cdc1d56c
endoKazuki <enkazu1116@gmail.com>
2025-09-28 23:40:02 +0900

    Spring WebFluxとSpring Bootを併用したアーキテクチャの試作とミドルウェアの導入 LiteSpeedを使用してリバースプロキシまでを行う

---
+++ src/main/java/com/endo1116/combinationSpring/CombinationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.endo1116.combinationSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombinationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombinationSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endo1116/combinationSpring/CombinationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endo1116.combinationSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombinationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
