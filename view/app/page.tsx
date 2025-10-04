e558a8b4eef7ed1515dd61139dada0c16ea4e647
endoKazuki <enkazu1116@gmail.com>
2025-10-04 23:53:29 +0900

    useEffectの試運転

--- view/app/page.tsx
+++ view/app/page.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [ count, setCount ] = useState(0);
   const handleClick = () => setCount(count + 1);
+
+  const handleWheel = e => e.preventDefalut();
+  const divRef = useRef(null);
+  useEffect(() => {
+    const div = divRef.current;
+    div.addEventListener('wheel', handleWheel. { passive: false });
+    return (() => {
+      div.removeEventListner('wheel', handleWheel);
+    });
+  })
 
   return (
     <>
